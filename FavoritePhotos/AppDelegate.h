38d4deba992447c62dc26e8e1e5844ab9863f213
DanielSon105 <daniel.g.barrido@gmail.com>
2015-10-15 16:06:47 -0500

    Initial Commit

---
+++ FavoritePhotos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FavoritePhotos
+//
+//  Created by Daniel Barrido on 10/15/15.
+//  Copyright © 2015 Daniel Barrido. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
